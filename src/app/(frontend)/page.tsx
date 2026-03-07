30d3267206379bce0f1cd9f3e69279aac64d2b51
Archan Lathiya <97425362+archanlathiya@users.noreply.github.com>
2026-03-07 16:29:38 +0530

    Update page.tsx

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -25,7 +25,7 @@ export default async function HomePage() {
       <AboutSection />
       <ServicesSection />
       <FeaturesSection />
-      <ProjectsSection />
+      {/* <ProjectsSection /> */}
       <ClientsSection />
     </div>
   )
